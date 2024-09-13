dd48be0d87fcf78a8a1c43981c1ca2322ee32130
VenkataGovindMuthamsetty <vgovind20@gmail.com>
2024-09-13 12:41:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Jobs from './components/Jobs/Jobs';
+import JobDetails from './components/JobDetails/JobDetails';
+import Bookmarks from './components/Bookmarks/Bookmarks';
+import Home from './components/Home/Home'
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/jobs' element={<Jobs/>}/>
+        <Route path="/job/:id" element={<JobDetails />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
